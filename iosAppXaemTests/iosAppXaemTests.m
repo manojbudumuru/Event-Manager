043bb6df2ce057380934a4f9c55e8728a8850f5e
Student <student@chimac-77.nwmissouri.edu>
2014-12-01 19:16:00 -0600

    Initial Commit

---
+++ iosAppXaemTests/iosAppXaemTests.m
@@ -0,0 +1,34 @@
+//
+//  iosAppXaemTests.m
+//  iosAppXaemTests
+//
+//  Created by Student on 12/1/14.
+//  Copyright (c) 2014 Student. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iosAppXaemTests : XCTestCase
+
+@end
+
+@implementation iosAppXaemTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
